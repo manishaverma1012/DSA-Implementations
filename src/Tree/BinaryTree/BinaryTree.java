92646393f181517b16e95b2ed43eeb8785ab0495
1012Maani <manishaverma140698@gmail.com>
2023-08-14 16:04:00 +0530

    tree implementation

---
+++ src/Tree/BinaryTree/BinaryTree.java
@@ -0,0 +1,117 @@
+package Tree.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BT
+    {
+        static int idx=-1; //this will use for indexing part;
+        public static Node buildTree(int nodes[])
+        {
+            idx++;
+            if(nodes[idx]==-1)
+            {
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+        public static void preOrderTraversal(Node root)
+        {
+            if(root==null)
+            {
+                return;
+            }
+            System.out.println(root.data+ " ");
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+
+
+        }
+        public static void InOrderTraversal(Node root)
+        {
+            if(root==null)
+            {
+                return;
+            }
+            InOrderTraversal(root.left);
+            System.out.println(root.data+" ");
+            InOrderTraversal(root.right);
+        }
+        public static void PostOrderTraversal(Node root)
+        {
+            if(root==null)
+            {
+                return;
+            }
+            InOrderTraversal(root.left);
+            InOrderTraversal(root.right);
+            System.out.println(root.data+" ");
+        }
+        public static void LevelOrderTraversal(Node root)
+        {
+            //queue data structure and iterating
+            //BFS
+            if(root==null)
+            {
+                return;
+            }
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty())
+            {
+                Node currNode=q.remove();
+                if(currNode==null)
+                {
+                    System.out.println();
+                    if(q.isEmpty())
+                    {
+                        break;
+                    }
+                    else {
+                        q.add(null);
+                    }
+
+                }
+                else {
+                    System.out.println(currNode.data+ " ");
+                    if(currNode.left!=null)
+                    {
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null)
+                    {
+                        q.add(currNode.right);
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BT tree=new BT();
+        Node root=tree.buildTree(nodes);
+        tree.LevelOrderTraversal(root);
+
+    }
+}
