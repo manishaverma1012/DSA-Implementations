1e8b49f2c4a646c2fff4948ce045122cce89e993
1012Maani <manishaverma140698@gmail.com>
2023-08-22 12:23:02 +0530

    deque done

--- src/Linkedlist/CLL.java
+++ src/Linkedlist/CLL.java
@@ -1,4 +1,127 @@
 package Linkedlist;
 
 public class CLL {
+    static class Node {
+        int data;
+        Node next;
+    }
+    static Node insertToEmpty(Node last, int data) {
+        if (last != null)
+            return last;
+
+        // allocate memory to the new node
+        Node newNode = new Node();
+
+        // assign data to the new node
+        newNode.data = data;
+
+        // assign last to newNode
+        last = newNode;
+
+        // create link to iteself
+        newNode.next = last;
+
+        return last;
+    }
+    static Node insertFront(Node last, int data) {
+        if (last == null)
+            return insertToEmpty(last,data);
+
+        Node newNode=new Node();
+
+        // add data to the node
+        newNode.data = data;
+
+        // store the address of the current first node in the newNode
+        newNode.next = last.next;
+
+        // make newNode as head
+        last.next = newNode;
+
+        return last;
+    }
+    static Node insertEnd(Node last, int data) {
+        if (last == null)
+            return insertToEmpty(last, data);
+
+        // allocate memory to the new node
+        Node newNode = new Node();
+
+        // add data to the node
+        newNode.data = data;
+
+        // store the address of the head node to next of newNode
+        newNode.next = last.next;
+
+        // point the current last node to the newNode
+        last.next = newNode;
+
+        // make newNode as the last node
+        last = newNode;
+
+        return last;
+    }
+    static Node deleteNode(Node last, int key) {
+        // if linked list is empty
+        if (last == null)
+            return null;
+
+        // if the list contains only a single node
+        if (last.data == key && last.next == last) {
+            last = null;
+            return last;
+        }
+
+        Node temp = last, d = new Node();
+
+        // if last is to be deleted
+        if (last.data == key) {
+            // find the node before the last node
+            while (temp.next != last) {
+                temp = temp.next;
+            }
+
+            // point temp node to the next of last i.e. first node
+            temp.next = last.next;
+            last = temp.next;
+        }
+
+        // travel to the node to be deleted
+        while (temp.next != last && temp.next.data != key) {
+            temp = temp.next;
+        }
+
+        // if node to be deleted was found
+        if (temp.next.data == key) {
+            d = temp.next;
+            temp.next = d.next;
+        }
+        return last;
+    }
+    static void Display(Node last) {
+        Node temp;
+
+        if (last == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        temp = last.next;
+
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+
+        }
+        while (temp != last.next);
+
+    }
+
+    public static void main(String[] args) {
+        Node last=null;
+        last=insertToEmpty(last,3);
+
+    }
+
+
 }
--- src/Queue/Deque.java
+++ src/Queue/Deque.java
@@ -1,4 +1,158 @@
 package Queue;
-
+//implementation of deque by using circular array;
+// we can implement by using doubly linkedlist as well
+//https://www.geeksforgeeks.org/implementation-deque-using-circular-array/
 public class Deque {
+    static final int max =1000;
+    int arr[];
+    int front;
+    int rear;
+    int size;
+    public Deque(int size)
+    {
+        arr=new int[max];
+        front=-1;
+        rear=0;
+        this.size=size;
+    }
+    public boolean isFull()
+    {
+        if(front==0 && rear==size-1 || front==rear+1)
+        {
+            return true;
+
+        }
+        return false;
+
+    }
+    public boolean isEmpty()
+    {
+        return (front==-1);
+
+    }
+    public void addFront(int key)
+    {
+        if (isFull()) {
+            System.out.println("Overflow");
+            return;
+        }
+        if(front==-1)
+        {
+            front=0;
+            rear=0;
+
+        }
+        else if(front==0)
+        {
+            front=size-1;
+        }
+        else {
+            front =front-1;
+
+        }
+        arr[front]=key;
+
+
+
+
+
+
+    }
+    public void addLast(int key)
+    {
+        if(isFull())
+        {
+            System.out.println("can not insert it");
+            return;
+        }
+        if(front==-1)
+        {
+            front=0;
+            rear=0;
+        }
+        else if( rear==size-1)
+        {
+            rear=0;
+        }
+        else {
+            rear=rear+1;
+        }
+        arr[rear]=key;
+
+    }
+    public void deleteFront()
+    {
+        if (isEmpty()) {
+            System.out.println("Queue Underflow\n");
+            return;
+        }
+        // Deque has only one element
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        }
+
+            // back to initial position
+        else if (front == size - 1)
+        {
+                front = 0;
+        }
+
+            else
+        {
+            front = front + 1;
+        }
+
+
+            // increment front by '1' to remove current
+
+                // front value from Deque
+
+    }
+    public void deleteLast()
+    {
+        if (isEmpty()) {
+            System.out.println("Queue Underflow\n");
+            return;
+        }
+        // Deque has only one element
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        }
+        else if(rear==0)
+        {
+            rear=size-1;
+
+        }
+        else {
+            rear=rear-1;
+
+        }
+
+    }
+    public int getFront()
+    {
+        if (isEmpty()) {
+            System.out.println(" Underflow");
+            return -1;
+        }
+        return arr[front];
+
+    }
+    public int getLast()
+    {
+        if (isEmpty() || rear < 0) {
+            System.out.println(" Underflow\n");
+            return -1;
+        }
+        return arr[rear];
+
+    }
+
+    public static void main(String[] args) {
+        Deque dq=new Deque(7);
+    }
+
+
 }
