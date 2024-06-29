21782c6b454983981cc246308c8bf5e1f7506055
1012Maani <manishaverma140698@gmail.com>
2024-06-29 16:32:52 +0530

    working on Sets

--- src/Collections/Set/HashSet/Main.java
+++ src/Collections/Set/HashSet/Main.java
@@ -1,15 +1,29 @@
 package Collections.Set.HashSet;
 
 public class Main {
-    public static void main(String[] args) {
-        MyHashSet set=new MyHashSet();
-        set.add(3);
-        set.add(4);
-        set.add(5);
-        set.add(9);
-        set.add(3);
-        System.out.println(set.contains(4));
+//    public static void main(String[] args) {
+//        MyHashSet set=new MyHashSet();
+//        set.add(3);
+//        set.add(4);
+//        set.add(5);
+//        set.add(9);
+//        set.add(3);
+//        System.out.println(set.contains(4));
+//
+//
+//    }
 
+            public static void main(String[] args) {
+            GenericHashSet<Integer> HashSet = new GenericHashSet<>();
+            HashSet.add(1);
+            HashSet.add(2);
+            HashSet.add(3);
 
-    }
+            System.out.println("Contains 2: " + HashSet.contains(2)); // true
+            System.out.println("Contains 4: " + HashSet.contains(4)); // false
+
+            HashSet.remove(2);
+            System.out.println("Contains 2 after removal: " + HashSet.contains(2)); // false
+            System.out.println("Size: " + HashSet.size()); // 2
+        }
 }
