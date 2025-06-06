ffbb4a73d6200771da15d4539b14983b944bc9c0
manishaverma1012 <manishaverma140698@gmail.com>
2025-06-06 11:20:54 +0530

    adding few comments in binary search file

--- src/Array/Binary_Search/Binarysearch.java
+++ src/Array/Binary_Search/Binarysearch.java
@@ -6,14 +6,17 @@ public class Binarysearch {
         int target= 65;
         int result=binarysearch(arr,target);
         System.out.println(result);
-
     }
+    // want to return index
+    // will apply only sorted array
+    //if left and right are both large (near INT_MAX), then left + right can overflow the range of a 32-bit integer before you divide by 2.
     public static int binarysearch(int arr[] ,int target)
     {
         int left=0;
         int right=arr.length-1;
         while(left<=right)
         {
+            //To avoid that overflow, you rewrite it as: right – left is the length of the interval. Even if left and right are near INT_MAX, their difference is guaranteed to be ≤ INT_MAX.
             int mid=left+(right-left)/2;
             if(arr[mid]==target)
             {
